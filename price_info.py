d865e734829458091b7d86f5f694b4ece37797f5
Vrishank <Kesavan_Vrishank_Raghav@ichat.sp.edu.sg>
2025-11-30 23:32:30 +0800

    tytyty

--- price_info.py
+++ price_info.py
@@ -8,9 +8,11 @@ def total_cost_shopping():
     total_cost = 0
     for key in price_list.keys():
         if key in quantity_list:
-            # complete the implementation below:
+            total_cost += quantity_list[key]*price_list[key]# complete the implementation below:
 
     print("total cost = ", total_cost)
+
+    return total_cost
 
 
 def cost_of_fruits(fruit, quantity):
@@ -20,6 +22,8 @@ def cost_of_fruits(fruit, quantity):
             break
 
     print("cost of ", quantity, fruit, "=", cost)
+
+    return cost
 
 
 def main():
